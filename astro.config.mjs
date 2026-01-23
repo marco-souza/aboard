1d4285f8f7ffd7400735d646b3256343bebafb3b
Marco Souza <marco.souza@mongodb.com>
2026-01-23 18:23:16 -0300

    fea: set up solid + search bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
 
-	adapter: cloudflare(),
+  adapter: cloudflare(),
+  integrations: [solidJs()],
 });
