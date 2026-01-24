353cbf85b1af8bec1b2205a8c7acf6772ca1d5f4
Marco Souza <marco.souza@mongodb.com>
2026-01-24 12:42:09 -0300

    fea: add app config

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+import { z } from "astro:schema";
+
+const AppConfigSchema = z.object({
+  BASE_URL: z.string().url(),
+  GOOGLE_AUTH_KEY: z.string().min(1),
+  GITHUB_AUTH_KEY: z.string().min(1),
+})
+
+export const config = AppConfigSchema.parse(Bun.env)
