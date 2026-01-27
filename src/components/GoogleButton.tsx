769b7769855621e972af920d2546e0f159d18345
Marco Souza <marco.souza@mongodb.com>
2026-01-26 21:14:38 -0300

    fix: svg issues

--- src/components/GoogleButton.tsx
+++ src/components/GoogleButton.tsx
@@ -2,6 +2,7 @@ export default function GoogleButton() {
   return (
     <a href="/api/auth/google" class="btn btn-outline btn-lg w-full gap-2">
       <svg class="w-5 h-5" viewBox="0 0 24 24">
+        <title>Google</title>
         <path
           fill="currentColor"
           d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
