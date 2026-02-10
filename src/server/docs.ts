4052f3177c546e55c4521f64a15b11f39e4be90b
Marco Souza <marco.souza@mongodb.com>
2026-02-10 10:29:25 -0300

    fea: add openapi endpoints

--- src/server/docs.ts
+++ src/server/docs.ts
@@ -1,26 +1,28 @@
 import { createRoute, type OpenAPIHono, z } from "@hono/zod-openapi";
 import { Scalar } from "@scalar/hono-api-reference";
 
+const docsResponseSchema = z
+  .object({
+    openapi: z.string(),
+    info: z.object({
+      title: z.string(),
+      version: z.string(),
+    }),
+  })
+  .loose();
+
 export function configureDocs(app: OpenAPIHono) {
   // Serve the OpenAPI JSON
   app.openapi(
     createRoute({
       method: "get",
-      path: "/docs",
+      path: "/openapi",
       responses: {
         200: {
           description: "OpenAPI Specification JSON",
           content: {
             "application/json": {
-              schema: z
-                .object({
-                  openapi: z.string(),
-                  info: z.object({
-                    title: z.string(),
-                    version: z.string(),
-                  }),
-                })
-                .loose(),
+              schema: docsResponseSchema,
             },
           },
         },
@@ -40,10 +42,10 @@ export function configureDocs(app: OpenAPIHono) {
 
   // Serve the Scalar UI
   app.get(
-    "/docs/ui",
+    "/openapi/ui",
     Scalar({
       defaultOpenAllTags: true,
-      url: "/api/docs",
+      url: "/api/openapi",
     }),
   );
 }
