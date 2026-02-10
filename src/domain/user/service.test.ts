ed0de10db5440ab77a331c037b12727204e1eac4
Marco Souza <marco.souza@mongodb.com>
2026-02-10 13:51:53 -0300

    fix: update service test cases

--- src/domain/user/service.test.ts
+++ src/domain/user/service.test.ts
@@ -1,7 +1,12 @@
-import { describe, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import { UserService } from "./service";
 
 describe("validate service was created", () => {
-  expect(UserService).not.toBeNull();
-  expect(UserService.userSchema).not.toBeNull();
+  it("has object", () => {
+    expect(UserService).not.toBeNull();
+  });
+
+  it("has user schema", () => {
+    expect(UserService.userSchema).not.toBeNull();
+  });
 });
